c4fdcee4bc40fc10badddb6eea99a2a37176920c
Aayush <aayushmishra4@gmail.com>
2022-11-14 12:02:48 +0530

    first commit

---
+++ src/comps/Title.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Title = () => {
+  return (
+    <div className="title">
+      <h1>Fotugilheri</h1>
+      <h2>Your Pictures</h2>
+      <p>Don't be shy it's your own private gallery.</p>
+    </div>
+  )
+}
+
+export default Title;
---
+++ src/firebase/config.js
@@ -0,0 +1,22 @@
+import * as firebase from 'firebase/app';
+import 'firebase/storage';
+import 'firebase/firestore';
+
+ // Your web app's Firebase configuration
+ var firebaseConfig = {
+    apiKey: "AIzaSyC_2phxobRJuZzrxVy2mgmiaSID0F6O07k",
+    authDomain: "fotugilheri.firebaseapp.com",
+    projectId: "fotugilheri",
+    storageBucket: "fotugilheri.appspot.com",
+    messagingSenderId: "690045049857",
+    appId: "1:690045049857:web:6d02b96c25b6edec0c3400"
+  };
+
+ // Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+const projectStorage = firebase.storage();
+const projectFirestore = firebase.firestore();
+const timestamp = firebase.firestore.FieldValue.serverTimestamp;
+
+export { projectStorage, projectFirestore, timestamp };
